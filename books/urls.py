8ba3f4f1e63591e231a2b4af21fbe6df9105a99c
Lyda Caballero <lidaevelyn@outlook.com>
2017-09-24 21:42:47 -0400

    add a line in the url file

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
-<<<<<<< HEAD
-from . import views #line added by lida
+from . import views 
+from users import urls as users_urls
 
 urlpatterns = [
     # /books/  this will show a page with all the Books
@@ -9,10 +9,3 @@ urlpatterns = [
     # /books/05/ this will show a page with the details od the views
     url(r'(?P<book_id>[0-9]+)/$', views.book_detail, name = 'detail'),
 ]
-=======
-from users import urls as users_urls
-
-urlpatterns = [
-    
-]
->>>>>>> c152504a46e558e1b2f18d4e76ac8f9e6bcddb51
