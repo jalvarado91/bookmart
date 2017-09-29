652fd92e1b9dfdeb81c5f21c503c0bb3e0ce0b61
Juan Alvarado <jalvarado91@gmail.com>
2017-09-29 16:48:51 -0400

    book details styles

--- books/urls.py
+++ books/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url, include
 from . import views 
 from users import urls as users_urls
 
+app_name = 'books'
 urlpatterns = [
     # /books/  this will show a page with all the Books
-    url(r'^$',views.index, name = 'index'),
-
+    url(r'^$', views.index, name='index'),
     # /books/05/ this will show a page with the details od the views
-    url(r'(?P<book_id>[0-9]+)/$', views.book_detail, name = 'detail'),
+    url(r'(?P<book_id>[0-9]+)/$', views.book_detail, name='detail'),
 ]
