3bba91a33c1a921812c9817e7dbdf3b13098e48b
Juan Alvarado <jalvarado91@gmail.com>
2017-10-09 18:12:32 -0400

    fix detail styles

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,42 +1,35 @@
 (function($) {
-    "use strict";
+  "use strict";
 
-    $('body').scrollspy({
-        target: '.fixed-top',
-        offset: 60
+  $("body").scrollspy({
+    target: ".fixed-top",
+    offset: 60
+  });
+
+  $("#collapsingNavbar li a").click(function() {
+    /* always close responsive nav after click */
+    $(".navbar-toggler:visible").click();
+  });
+
+  /*lida code for phot popup*/
+  $(".col-sm-3 img").click(function() {
+    $(".show").fadeIn();
+  });
+
+  $("#close-overlay, .overlay").click(function() {
+    $(".show").fadeOut();
+  });
+
+  $(".tab-panels .tabs li a").on("click", function() {
+    $(".tab-panels .tabs li a.active").removeClass("active");
+    $(this).addClass("active");
+
+    var showPanel = $(this).attr("rel");
+
+    $(".tab-panels .panel.active").hide(0, function() {
+      $("#" + showPanel).show(0, function() {
+        $(this).addClass("active");
+      });
     });
-
-    $('#collapsingNavbar li a').click(function() {
-        /* always close responsive nav after click */
-        $('.navbar-toggler:visible').click();
-    });
-
-    /*lida code for phot popup*/
-    $(".col-sm-3 img").click(function () {
-        var $src = $(this).attr("src");
-        $(".show").fadeIn();
-        $(".img-show img").attr("src", $src);
-    });
-
-    $("span, .overlay").click(function () {
-      $(".show").fadeOut();
-    });
-
-
-      $('.tab-panels .tabs li').on('click', function () {
-          $('.tab-panels .tabs li.active').removeClass('active');
-          $(this).addClass('active');
-
-          var showPanel = $(this).attr('rel');
-
-          $('.tab-panels .panel.active').slideUp(300, function () {
-
-              $('#'+ showPanel).slideDown(300, function () {
-                  $(this).addClass('active');
-              });
-          });
-       });
-
-
-
+  });
 })(jQuery);
