3f362e71cc182bd75a52dc353aa569234c9f2783
Ronny Alfonso <roncareer76@yahoo.com>
2017-10-12 21:06:29 -0400

    more changes that werent added to the last commit

--- books/models.py
+++ books/models.py
@@ -3,22 +3,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Author(models.Model):
-	name = models.CharField(max_length=100)
-	bio = models.TextField()
-	def __str__(self):
-		return self.name
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
-	title = models.CharField(max_length=100)
-	author = models.ManyToManyField(Author)
-	cover_url = models.CharField(max_length=200)
-	description = models.TextField()
-	genre = models.CharField(max_length=200)
-	price = models.FloatField()
-	rating = models.IntegerField(blank=True, null=True)
-	publisher = models.CharField(max_length=100)
-	release_date = models.DateTimeField('publishing date')
+    title = models.CharField(max_length=100)
+    author = models.ManyToManyField(Author)
+    cover_url = models.CharField(max_length=200)
+    description = models.TextField()
+    genre = models.CharField(max_length=200)
+    price = models.FloatField()
+    rating = models.IntegerField(blank=True, null=True)
+    publisher = models.CharField(max_length=100)
+    release_date = models.DateTimeField('publishing date')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
