d7c8fe2c60a52f5eebebd1b4a80019e772b6f708
Juan Alvarado <jalvarado91@gmail.com>
2017-10-14 13:25:54 -0400

    Display star ratings and reviews in book details page.

--- books/models.py
+++ books/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth import settings
 
 
 class Author(models.Model):
@@ -19,9 +20,18 @@ class Book(models.Model):
     description = models.TextField()
     genre = models.CharField(max_length=200)
     price = models.FloatField()
-    rating = models.IntegerField(blank=True, null=True)
     publisher = models.CharField(max_length=100)
     release_date = models.DateTimeField('publishing date')
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    rating = models.PositiveIntegerField(blank=True, null=True)    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'review for {0}: rating {1}'.format(self.book, self.rating)
--- books/views.py
+++ books/views.py
@@ -2,21 +2,71 @@
 from __future__ import unicode_literals
 from django.http import Http404
 from django.http import HttpResponse
+from django.db.models import Count, Avg
+import math
+
 from .models import Book
 from .models import Author
 from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):  #this method defined by lida
+def index(request):  # this method defined by lida
     all_books = Book.objects.all()
     return render(request, 'book/index1.html', {'all_books': all_books})
-    #return render(request, 'book/index2.html', context)
+    # return render(request, 'book/index2.html', context)
 
 
 def book_detail(request, book_id):
     try:
         book = Book.objects.get(pk=book_id)
+
+        # reviews
+        reviews = book.review_set.all().order_by('-created_at')
+        reviews_count = book.review_set.count()
+        
+        avg_rating = book.review_set.aggregate(Avg('rating'))['rating__avg']
+        rating_count = book.review_set.aggregate(Count('rating'))['rating__count']
+
+        if rating_count > 0:
+            range_pos = int(math.floor(avg_rating))
+            rating_filled = range(range_pos)
+            rating_empty = range(5 - range_pos)
+        else: 
+            rating_filled = None
+            rating_empty = None
+        
+
+        templ_context = {
+            'book': book,
+            'review_objs': get_review_stats(reviews),
+            'reviews_count': reviews_count,
+            'rating_filled': rating_filled,
+            'rating_empty': rating_empty,
+            'avg_rating': avg_rating,
+            'rating_count': rating_count
+        }
+
     except Book.DoesNotExist:
         raise Http404("Book does not exist")
-    return render(request, 'book/detail.html', {'book': book})
+    return render(request, 'book/detail.html', templ_context)
+
+
+def get_review_stats(reviews):
+    aggs = []
+    for review in reviews:
+        if review.rating == None:
+            aggs.append((
+                review,
+                None
+            ))
+        else: 
+            aggs.append((
+                review,
+                {
+                    "filled": range(review.rating),
+                    "empty": range(5 - review.rating)
+                }
+            ))
+    print aggs
+    return aggs
