8832485acf33e95e30a679e63ca9937ec5c08b19
mbarr203 <31226202+mbarr203@users.noreply.github.com>
2017-11-04 16:15:33 -0400

    Add "Save for Later" functionality in shopping cart

--- carts/urls.py
+++ carts/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     url(r'^$', views.detail, name='detail'),
     url(r'^add/(?P<book_id>\d+)/$', views.cart_add, name='add'),
     url(r'^remove/(?P<book_id>\d+)/$', views.cart_remove, name='cart_remove'),
+    url(r'^save/(?P<book_id>\d+)/$', views.save_for_later, name='save_for_later'),
+    url(r'^addBackToCart(?P<savedItems_id>\d+)/$', views.move_to_cart, name='move_to_cart'),
+    url(r'^(?P<savedItems_id>\d+)/$', views.delete_from_saved, name='delete_from_saved')
+
     ]
