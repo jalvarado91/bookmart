b2198f12951cb047f868f22d6f9dde01164159c7
Orlando <oarno004@fiu.edu>
2017-11-13 13:14:14 -0500

    implements Genre model, and genre list with proper links, seed file to reflect changes

--- books/models.py
+++ books/models.py
@@ -13,12 +13,22 @@ class Author(models.Model):
         return self.name
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ManyToManyField(Author)
     cover_url = models.CharField(max_length=200)
     description = models.TextField()
-    genre = models.CharField(max_length=200)
+    genre = models.ManyToManyField(Genre)
     price = models.FloatField()
     publisher = models.CharField(max_length=100)
     release_date = models.DateTimeField('publishing date')
